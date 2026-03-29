a4ae5c52b7ebb33b7e00eb6d8590baea88bf0fbc
Sonusree Sarkar <sarkar.sonu1904@gmail.com>
2026-03-29 12:34:32 +0530

    Added Admin class using inheritance and polymorphism

---
+++ src/com/library/model/Admin.java
@@ -0,0 +1,13 @@
+package com.library.model;
+
+public class Admin extends User {
+
+    public Admin(int id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void showRole() {
+        System.out.println("Admin");
+    }
+}
